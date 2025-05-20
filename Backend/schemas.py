251198b7f0dae333d60760598906a00c785a4a55
Renan Domingos <rna.domingos@hotmail.com>
2025-05-19 23:37:05 -0300

    feat: add schemas

---
+++ Backend/schemas.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, PositiveFloat, PositiveInt, EmailStr
+from datetime import datetime
+from typing import Optional
+
+class ProductBase(BaseModel):
+    name: str
+    description: str
+    price: PositiveFloat
+    category: str
+    supplier_email: EmailStr
+
+class ProductCreate(ProductBase):
+    pass
+
+class ProductResponse(ProductBase):
+    id: PositiveInt
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
+
+class ProductUpdate(BaseModel):
+    name: Optional[str]
+    description: Optional[str]
+    price: Optional[PositiveFloat]
+    category: Optional[str]
+    supplier_email: Optional[EmailStr]
