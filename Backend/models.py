6251623ee8edf99f9d421efaac38d2958c7ef6d2
Renan Domingos <rna.domingos@hotmail.com>
2025-05-19 22:51:01 -0300

    feat: add ProductModel

---
+++ Backend/models.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, Float, String, DateTime
+from sqlalchemy.sql import func
+from database import Base
+
+class ProductModel(Base):
+    __tablename__ = 'products'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    description = Column(String)
+    price = Column(Float)
+    category = Column(String)
+    supplier_email = Column(String)
+    created_at = Column(DateTime(timezone=True), default=func.now())
+
+    
+     
+
+
+
