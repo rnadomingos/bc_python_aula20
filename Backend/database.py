d640e71c8295a5f4595d3c3f5847e7c0868f829a
Renan Domingos <rna.domingos@hotmail.com>
2025-05-19 22:34:52 -0300

    feat: add doc string on database

--- Backend/database.py
+++ Backend/database.py
@@ -11,6 +11,9 @@ SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
+    '''
+    Função get_db cria a conexão e gera a sessão
+    '''
     db = SessionLocal()
     try:
         yield db
